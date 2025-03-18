a679ad7cef0ff81d3244c03f5606737e5a2d3420
rajibr <82666308+rajibrajkhowa@users.noreply.github.com>
2025-03-18 15:07:45 +0530

    Create f5_pool_status.sh

---
+++ f5_pool_status.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Variables Passed As Arguments
+F5_USER=$1
+F5_HOST=$2
+F5_PASS=$(<password.txt)
+POOL=$3
+
+# Function to get the status of a pool
+get_pool_status() {
+    curl -sk -u $F5_USER:$F5_PASS \
+    -H "Content-Type: application/json" \
+    -X GET "https://$F5_HOST/mgmt/tm/ltm/pool/~Common~$POOL/stats"
+}
+
+echo
+
+# Call the function
+POOL_AVAILABILITY_STATUS=$(get_pool_status | jq '.entries[].nestedStats.entries | ."status.availabilityState"' | jq '.description' | tr -d '"')
+
+POOL_STATUS=$(get_pool_status | jq '.entries[].nestedStats.entries | ."status.enabledState"' | jq '.description' | tr -d '"')
+
+jq -nc \
+ --arg lb_host "$F5_HOST"\
+ --arg pool "$POOL" \
+ --arg availability "$POOL_AVAILABILITY_STATUS" \
+ --arg pool_status "$POOL_STATUS" \
+ '{
+    "LB HOST": $lb_host,
+    "POOL": $pool,
+    "POOL_AVAILABILITY_STATUS": $availability,
+    "POOL_STATUS": $pool_status
+  }'
